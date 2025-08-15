99156967965f4a6abe5e08f87b954f35681814f3
zorakisgeorge <zorakisgeorge@gmail.com>
2025-08-15 15:20:09 +0300

    Make header responsive add hamburger menu

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,13 +1,87 @@
-import Logo from "./Logo";
-import Navigation from "./Navigation";
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo_agrodynamiki.png";
+import { HiMenu, HiX } from "react-icons/hi";
+import { useState } from "react";
 
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  function handleMenu() {
+    setMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <header className="border-b border-primary-900 px-8 py-5">
-      <div className="flex justify-between items-center max-w-7xl mx-auto">
-        <Logo />
-        <Navigation />
+    <header className="border-b border-primary-900 px-8 py-5 z-20 bg-custom-bg1 text-stone-700 font-semibold ">
+      <div className="flex justify-between max-w-7xl mx-auto items-center container">
+        <Link href="/">
+          <Image className="min-w-[200px]" src={logo} alt="Agrodynamiki logo" />
+        </Link>
+        <nav className="z-10 text-xl">
+          <ul className="lg:flex hidden gap-8 lg:gap-16 items-center">
+            <li>
+              <Link href="/services" className="hover:text-stone-500">
+                Υπηρεσίες
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="hover:text-stone-500">
+                Προϊόντα
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="hover:text-stone-500">
+                Αγροδυναμική
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="hover:text-stone-500">
+                Επικοινωνία
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Small screen */}
+        <div className="lg:hidden cursor-pointer">
+          {isMenuOpen ? (
+            <HiX size={30} onClick={handleMenu} />
+          ) : (
+            <HiMenu size={30} onClick={handleMenu} />
+          )}
+        </div>
       </div>
+
+      <nav className="z-10 text-xl lg:hidden">
+        <div
+          className={`overflow-hidden transition-[max-height] duration-500  [will-change:max-height]
+      ${isMenuOpen ? "max-h-96" : "max-h-0"}`}
+        >
+          <ul className="flex flex-col gap-8 mt-8 text-center">
+            <li>
+              <Link href="/services" className="hover:text-stone-500">
+                Υπηρεσίες
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="hover:text-stone-500">
+                Προϊόντα
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="hover:text-stone-500">
+                Αγροδυναμική
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="hover:text-stone-500">
+                Επικοινωνία
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import bg from "@/public/bg.jpg";
+
 export default function Page() {
-  return <h1>HELLO NEXT</h1>;
+  return (
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        placeholder="blur"
+        quality={80}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center  md:mt-[-20]">
+        <h1 className="sm:text-6xl text-3xl  text-primary-100 mb-1 tracking-tight font-normal">
+          <span className="text-accent-500">ΑΓΡΟ</span>
+          ΔΥΝΑΜΙΚΗ
+        </h1>
+        <h2 className="sm:text-2xl text-primary-100">
+          Ανδρουλάκης Ελευθέριος & ΣΙΑ Ο.Ε.
+        </h2>
+        {/* <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link> */}
+      </div>
+    </main>
+  );
 }
